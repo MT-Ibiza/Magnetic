faa7ab1b2049643e1ab8822eae455ba4c9e54c94
Lenin Capa <leviskp87@gmail.com>
2024-11-17 11:00:01 -0500

    minor changes in login page

--- src/app/[locale]/admin/login/page.tsx
+++ src/app/[locale]/admin/login/page.tsx
@@ -7,7 +7,16 @@ function Page(props: Props) {
   const {} = props
 
   return (
-    <LoginForm />
+    <div className="flex min-h-screen flex-col items-center justify-center lg:p-24">
+      <img
+        src="https://www.magnetic-travel.com/wp-content/uploads/2020/02/rsz_logo_mgtedit_.png"
+        className="h-full w-[170px] mb-[30px]"
+      />
+      {/* {error.length > 0 && (
+        <Text className="text-lg text-red-500 p-3 rounded mb-2">{error}</Text>
+      )} */}
+      <LoginForm/>
+    </div>
   )
 }
 
