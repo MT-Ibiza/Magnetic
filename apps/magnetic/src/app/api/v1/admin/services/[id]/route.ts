63fe574d2afc7912f7b25496e50b571e0a3c258d
Lenin Capa <leviskp87@gmail.com>
2024-11-28 22:36:26 -0500

    api services

---
+++ apps/magnetic/src/app/api/v1/admin/services/[id]/route.ts
@@ -0,0 +1,55 @@
+import db from 'apps/magnetic/src/app/libs/db';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const service = await db.service.findUnique({
+      where: {
+        id: Number(params.id),
+      },
+      include: {
+        items: true
+      }
+    });
+    return NextResponse.json(service);
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { name } = await request.json();
+  try {
+    const service = await db.service.update({
+      where: {
+        id: Number(params.id),
+      },
+      data: {
+        name: name,
+      },
+    });
+    return NextResponse.json(service, { status: 201 });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
---
+++ apps/magnetic/src/app/api/v1/admin/services/route.ts
@@ -0,0 +1,49 @@
+import db from 'apps/magnetic/src/app/libs/db';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  const { name, description, items, packageId, providerId } =
+    await request.json();
+
+  try {
+    const service = await db.service.create({
+      data: {
+        name: name,
+        description,
+        packageId,
+        providerId,
+        items: {
+          createMany: {
+            data: items,
+          },
+        },
+      },
+    });
+    return NextResponse.json(service);
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  try {
+    const services = await db.service.findMany();
+    return NextResponse.json({ services: services });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
