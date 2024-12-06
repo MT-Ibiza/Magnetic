94b564ac74347c910e695b0c0dc0ba0d7dc166c9
Lenin Capa <leviskp87@gmail.com>
2024-12-06 18:57:19 -0500

    client backend: add api get service

---
+++ apps/magnetic/src/app/api/v1/user/services/[id]/route.ts
@@ -0,0 +1,37 @@
+import db from 'apps/magnetic/src/app/libs/db';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const service = await db.service.findUnique({
+      where: {
+        id: Number(params.id),
+      },
+      include: {
+        items: {
+          orderBy: {
+            createdAt: 'desc',
+          },
+        },
+        package: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+    return NextResponse.json(service);
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
