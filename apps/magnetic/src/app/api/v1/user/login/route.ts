f04c3a483b3103e0bc5892f5f7c65649978307ed
Lenin Capa <leviskp87@gmail.com>
2024-12-06 17:13:25 -0500

    login user

---
+++ apps/magnetic/src/app/api/v1/user/login/route.ts
@@ -0,0 +1,59 @@
+import { NextRequest, NextResponse } from 'next/server';
+import bcrypt from 'bcrypt';
+import db from 'apps/magnetic/src/app/libs/db';
+import { signJwtAccessToken } from 'apps/magnetic/src/app/libs/jwt';
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { email, password } = body;
+  if (!email || !password) {
+    return new Response('Email and password are required', {
+      status: 400,
+    });
+  }
+
+  const client = await db.user.findUnique({
+    where: {
+      email: email,
+      role: 'client',
+    },
+  });
+
+  if (!client) {
+    return new Response('No user found', {
+      status: 404,
+    });
+  }
+
+  const matchPassword = await bcrypt.compare(password, client.password || '');
+
+  if (!matchPassword) {
+    return new Response('Wrong password', {
+      status: 401,
+    });
+  }
+
+  const accessToken = signJwtAccessToken({
+    id: client.id,
+    email: client.email,
+    role: client.role,
+  });
+
+  return NextResponse.json({
+    id: client.id,
+    name: client.name,
+    email: client.email,
+    image: client.image,
+    accessToken,
+  });
+}
+
+//cors error
+export async function GET(req: NextRequest) {
+  return NextResponse.json(
+    {
+      error: 'Nothing',
+    },
+    { status: 404 }
+  );
+}
