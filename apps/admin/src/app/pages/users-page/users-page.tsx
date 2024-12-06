f519dbcd6951aee8434c23feeb984c0aaea4b210
Lenin Capa <leviskp87@gmail.com>
2024-12-05 19:31:35 -0500

    add use client

--- apps/admin/src/app/pages/users-page/users-page.tsx
+++ apps/admin/src/app/pages/users-page/users-page.tsx
@@ -34,64 +34,7 @@ export function UsersPage() {
     },
   ];
 
-  return (
-    <>
-      <CardWrapper>
-        <>
-          <div className="header flex justify-between mb-8">
-            <div className="flex flex-col gap-1">
-              <h2>Users</h2>
-            </div>
-          </div>
-          <div className="overflow-x-auto">
-            <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-              <thead className="text-left">
-                <tr>
-                  <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                    ID
-                  </th>
-                  <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                    Name
-                  </th>
-                  <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                    Email
-                  </th>
-                  <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                    Date Created
-                  </th>
-                  <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                    Type
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-200">
-                {users.map((user) => (
-                  <tr key={user.id}>
-                    <td className="whitespace-nowrap px-4 py-4 font-medium text-gray-900">
-                      {user.id}
-                    </td>
-                    <td className="whitespace-nowrap px-4 py-4 text-gray-700">
-                      {user.name}
-                    </td>
-                    <td className="whitespace-nowrap px-4 py-4 text-gray-700">
-                      {user.email}
-                    </td>
-                    <td className="whitespace-nowrap px-4 py-4 text-gray-700">
-                      {user.dateCreated}
-                    </td>
-                    <td className="whitespace-nowrap px-4 py-4 text-gray-700">
-                      {user.type}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <UsersTable />
-        </>
-      </CardWrapper>
-    </>
-  );
+  return <UsersTable />;
 }
 
 export default UsersPage;
