9020acc47e7af3ae1a97f3f0451387de75e092b5
Angel Capa <ricardocmdn@gmail.com>
2024-12-05 19:26:03 -0500

    changes middleware

--- apps/magnetic/src/middleware.ts
+++ apps/magnetic/src/middleware.ts
@@ -1,28 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
+export function middleware() {
   // retrieve the current response
   const res = NextResponse.next();
-  const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN || '*';
 
   // add the CORS headers to the response
-  if (req.nextUrl.pathname.startsWith('/api')) {
-    res.headers.append('Access-Control-Allow-Credentials', 'true');
-    res.headers.append(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization'
-    );
-    res.headers.append('Access-Control-Allow-Origin', ALLOWED_ORIGIN);
-    res.headers.append(
-      'Access-Control-Allow-Methods',
-      'GET,DELETE,PATCH,POST,PUT'
-    );
-    res.headers.append(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-    return res;
-  }
+  res.headers.append('Access-Control-Allow-Credentials', 'true');
+  res.headers.append('Access-Control-Allow-Origin', '*'); // replace this your actual origin
+  res.headers.append(
+    'Access-Control-Allow-Methods',
+    'GET,DELETE,PATCH,POST,PUT'
+  );
+  res.headers.append(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
 
-  return NextResponse.next();
+  return res;
 }
+
+// specify the path regex to apply the middleware to
+export const config = {
+  matcher: '/api/:path*',
+};
