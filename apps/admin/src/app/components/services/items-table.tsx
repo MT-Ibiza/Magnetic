8aafdca6fd3d07f9d5c6763bba681eda0943c161
Lenin Capa <leviskp87@gmail.com>
2024-12-07 15:33:23 -0500

    just remove overflow class

--- apps/admin/src/app/components/services/items-table.tsx
+++ apps/admin/src/app/components/services/items-table.tsx
@@ -21,60 +21,58 @@ function ItemsTable(props: Props) {
   // }
 
   return (
-    <div className="overflow-x-auto">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>N</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Description</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item, index) => (
-            <tr className="hover">
-              <th>{index + 1}</th>
-              <td>
-                <Link to={`/services/${item.serviceId}/items/${item.id}/edit`}>
-                  {item.name}
-                </Link>
-              </td>
-              <td>{priceCentsToDollars(item.priceInCents)}</td>
-              <td>{item.description}</td>
-              <td>
-                <div className="dropdown dropdown-bottom dropdown-end">
-                  <div tabIndex={0} role="button" className="m-1">
-                    <HiOutlineDotsVertical />
-                  </div>
-                  <ul
-                    tabIndex={0}
-                    className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
-                  >
-                    <li
-                      onClick={() => {
-                        onClickEdit && onClickEdit(item);
-                      }}
-                    >
-                      <a>Edit</a>
-                    </li>
-
-                    <li
-                      onClick={() => {
-                        // onClickRemove && onClickRemove(user);
-                      }}
-                    >
-                      <a>Delete</a>
-                    </li>
-                  </ul>
+    <table className="table">
+      <thead>
+        <tr>
+          <th>N</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Description</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item, index) => (
+          <tr className="hover">
+            <th>{index + 1}</th>
+            <td>
+              <Link to={`/services/${item.serviceId}/items/${item.id}/edit`}>
+                {item.name}
+              </Link>
+            </td>
+            <td>{priceCentsToDollars(item.priceInCents)}</td>
+            <td>{item.description}</td>
+            <td>
+              <div className="dropdown dropdown-bottom dropdown-end">
+                <div tabIndex={0} role="button" className="m-1">
+                  <HiOutlineDotsVertical />
                 </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+                <ul
+                  tabIndex={0}
+                  className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+                >
+                  <li
+                    onClick={() => {
+                      onClickEdit && onClickEdit(item);
+                    }}
+                  >
+                    <a>Edit</a>
+                  </li>
+
+                  <li
+                    onClick={() => {
+                      // onClickRemove && onClickRemove(user);
+                    }}
+                  >
+                    <a>Delete</a>
+                  </li>
+                </ul>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
