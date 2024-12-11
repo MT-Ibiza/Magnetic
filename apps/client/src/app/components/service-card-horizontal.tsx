b1af42147ff7396e625939b8af95a1046800989c
Angel Capa <ricardocmdn@gmail.com>
2024-12-10 22:04:17 -0500

    client: add service card to dashboard

---
+++ apps/client/src/app/components/service-card-horizontal.tsx
@@ -0,0 +1,33 @@
+import { Service } from '@magnetic/interfaces';
+import { Text } from '@magnetic/ui';
+import React from 'react';
+
+interface Props {
+  service: Service;
+}
+
+function ServiceCardHorizontal(props: Props) {
+  const { service } = props;
+  const { name, description } = service;
+
+  return (
+    <div className="w-full relative flex items-center bg-base-100 border border-gray-300 rounded-lg overflow-hidden shadow-sm group hover:shadow-md transition duration-200 ease-in-out">
+      <div className="p-4 flex-1">
+        <h2 className="text-xl font-medium">{name}</h2>
+        <div
+          className="text-sm text-gray-500 line-clamp-2 mt-2"
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
+      </div>
+      <div className="flex justify-end w-1/4 bg-base-100">
+        <img
+          className="object-cover rounded-lg h-[125px] w-[125px]"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC8p9y72JP4pkbhibsAZkGeQU4ZL5Gp6L8VjYTvXgRvzm4t3xY2wbR5KFLOOQT5apKwv4&usqp=CAU"
+          alt={''}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ServiceCardHorizontal;
--- apps/client/src/app/pages/dashboard-page/dashboard-client.tsx
+++ apps/client/src/app/pages/dashboard-page/dashboard-client.tsx
@@ -1,6 +1,11 @@
 import { StatsCard } from '@magnetic/ui';
+import ServiceCardHorizontal from '../../components/service-card-horizontal';
+import { useServices } from '../../hooks/useServices';
+import { Link } from 'react-router-dom';
 
 export function DashboardClientPage() {
+  const { isLoading, services, error, isError } = useServices();
+
   const options = [
     {
       title: 'Total Guests',
@@ -30,6 +35,19 @@ export function DashboardClientPage() {
             percentage={option.number}
           />
         ))}
+      </div>
+      <div className="grid grid-cols-2 gap-y-[20px]">
+        <div className="flex flex-col gap-2">
+          {services.map((service, index) => (
+            <Link
+              key={service.id}
+              to={`/services/${service.id}`}
+              className="w-full"
+            >
+              <ServiceCardHorizontal service={service} />
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
