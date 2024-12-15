848e986bfbca2db6cb7b136eea2bb5797ce2c41b
Lenin Capa <leviskp87@gmail.com>
2024-12-14 21:40:10 -0500

    fix error

--- apps/admin/src/app/hooks/useCategories.ts
+++ apps/admin/src/app/hooks/useCategories.ts
@@ -1,6 +1,6 @@
 import { Category } from '@magnetic/interfaces';
 import { useQuery } from '@tanstack/react-query';
-import { getCategories } from '../apis/api-items';
+import { getCategories } from '../apis/api-categories';
 
 export const useCategories = () => {
   const { isLoading, isError, data, error, isSuccess, refetch } = useQuery<
