5cd27c70c2a2176814034bef5d8b32aeca969404
Lenin Capa <leviskp87@gmail.com>
2024-12-15 20:43:10 -0500

    remove component icon

--- libs/ui/src/lib/icon.tsx
+++
@@ -1,7 +0,0 @@
-import IcoMoon, { IconProps } from 'react-icomoon';
-import iconSet from '../assets/icons.json';
-
-export function Icon(props: IconProps) {
-  return <IcoMoon iconSet={iconSet} {...props} />;
-}
-export default Icon;
