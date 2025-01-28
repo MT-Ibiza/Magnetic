2dd5682c5e76554b975fe4fa4254348e9b16d509
Lenin Capa <leviskp87@gmail.com>
2025-01-27 21:58:32 -0500

    form request a call

---
+++ apps/client/src/app/components/form-request-call.tsx
@@ -0,0 +1,111 @@
+import { Button } from '@magnetic/ui';
+import { useForm } from 'react-hook-form';
+
+function FormRequestCall() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      date: null,
+      time: null,
+      notes: '',
+    },
+  });
+
+  const onSubmit = (data: any) => {
+    console.log('Form Data:', data);
+    alert('¡Llamada agendada exitosamente!');
+  };
+
+  return (
+    <div className="max-w-lg mx-auto rounded-lg p-3 bg-white">
+      <h2 className="text-2xl font-bold mb-4">Schedule a call</h2>
+      <p className="text-sm text-gray-500 mb-6">
+        Complete the form to schedule a call with a magnetic team member.
+      </p>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <div className="form-control">
+          <label htmlFor="name" className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Fullname"
+            className="input input-bordered"
+            {...register('name', { required: 'El nombre es obligatorio' })}
+          />
+          {errors.name && (
+            <p className="text-red-500 text-sm">{errors.name.message}</p>
+          )}
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="email" className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="youremail@mail.com"
+            className="input input-bordered"
+            {...register('email', {
+              required: 'Email is required',
+            })}
+          />
+          {errors.email && (
+            <p className="text-red-500 text-sm">{errors.email.message}</p>
+          )}
+        </div>
+        <div className="form-control">
+          <label htmlFor="date" className="label">
+            <span className="label-text">Fecha</span>
+          </label>
+          <input
+            id="date"
+            type="date"
+            className="input input-bordered"
+            {...register('date', { required: 'La fecha es obligatoria' })}
+          />
+          {errors.date && (
+            <p className="text-red-500 text-sm">{errors.date.message}</p>
+          )}
+        </div>
+        <div className="form-control">
+          <label htmlFor="time" className="label">
+            <span className="label-text">Time</span>
+          </label>
+          <input
+            id="time"
+            type="time"
+            className="input input-bordered"
+            {...register('time', { required: 'Time is required' })}
+          />
+          {errors.time && (
+            <p className="text-red-500 text-sm">{errors.time.message}</p>
+          )}
+        </div>
+        <div className="form-control">
+          <label htmlFor="notes" className="label">
+            <span className="label-text">Notes (Optional)</span>
+          </label>
+          <textarea
+            id="notes"
+            placeholder="Adicional details"
+            className="textarea textarea-bordered"
+            {...register('notes')}
+          />
+        </div>
+        <Button type="submit" className=" w-full">
+          Schedule Call
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default FormRequestCall;
--- apps/client/src/app/pages/packages-page/packages-page.tsx
+++ apps/client/src/app/pages/packages-page/packages-page.tsx
@@ -1,7 +1,9 @@
-import { CardWrapper } from '@magnetic/ui';
+import { Button, CardWrapper } from '@magnetic/ui';
 import { PlanCard } from '@magnetic/ui';
 import { usePackages } from '../../hooks/usePackages';
 import { useAuth } from '../../hooks/useAuth';
+import { Link } from 'react-router-dom';
+import FormRequestCall from '../../components/form-request-call';
 
 interface Package {
   id: number;
@@ -20,33 +22,56 @@ export function PackagePage() {
   );
 
   return (
-    <div className="bg-base-100 listingSection__wrap">
-      <div className="text-center mb-10">
-        <h1 className="text-2xl lg:text-3xl font-semibold">
-          Explore Our Packages
-        </h1>
-        <p className="text-base lg:text-lg mt-2">
-          Find the perfect package for your needs. Choose the one that best fits
-          your goals.
-        </p>
-      </div>
+    <>
+      <div className="bg-base-100 listingSection__wrap">
+        <div className="text-center mb-10">
+          <h1 className="text-2xl lg:text-3xl font-semibold">
+            Explore Our Packages
+          </h1>
+          <p className="text-base lg:text-lg mt-2">
+            Find the perfect package for your needs. Choose the one that best
+            fits your goals.
+          </p>
+          <Button
+            className="mt-3"
+            onClick={() => {
+              //@ts-ignore
+              document.getElementById('modal_form_call').showModal();
+            }}
+          >
+            Request a call
+          </Button>
+        </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {packages.map((pkg) => (
-          <PlanCard
-            key={pkg.id}
-            maxFeatures={3}
-            seeMoreLink={`/packages/${pkg.id}`}
-            title={pkg.name}
-            price={pkg.priceInCents}
-            features={pkg.features}
-            className={`${
-              pkg.id === user?.package?.id ? 'opacity-50' : ''
-            } transition-opacity duration-300`}
-          />
-        ))}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {packages.map((pkg) => (
+            <PlanCard
+              key={pkg.id}
+              maxFeatures={3}
+              seeMoreLink={`/packages/${pkg.id}`}
+              title={pkg.name}
+              price={pkg.priceInCents}
+              features={pkg.features}
+              className={`${
+                pkg.id === user?.package?.id ? 'opacity-50' : ''
+              } transition-opacity duration-300`}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+      <dialog id="modal_form_call" className="modal">
+        <div className="modal-box">
+          <FormRequestCall />
+          <form method="dialog">
+            <div className="flex px-3">
+              <Button className="w-full" variant="outline" color="neutral">
+                Close
+              </Button>
+            </div>
+          </form>
+        </div>
+      </dialog>
+    </>
   );
 }
 
