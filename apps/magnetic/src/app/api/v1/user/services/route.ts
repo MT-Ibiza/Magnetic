996898b4a76380f210f06e926c8518e9b89e9323
Lenin Capa <leviskp87@gmail.com>
2025-02-02 21:51:45 -0500

    sort package response

--- apps/magnetic/src/app/api/v1/user/services/route.ts
+++ apps/magnetic/src/app/api/v1/user/services/route.ts
@@ -15,6 +15,9 @@ export async function GET(request: Request) {
             id: true,
             name: true,
           },
+          orderBy: {
+            id: 'asc',
+          },
         },
       },
       orderBy: {
