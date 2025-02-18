ec742bd81dbf80d544344e794ae1e1a038bf577b
Lenin Capa <leviskp87@gmail.com>
2025-02-18 10:26:27 -0500

    center links

--- libs/ui/src/lib/header-client.tsx
+++ libs/ui/src/lib/header-client.tsx
@@ -58,43 +58,45 @@ export function HeaderClient(props: HeaderProps) {
       className={`bg-base-100 sticky top-0 left-0 right-0 z-20 shadow-sm lg:ml-0 w-full ${className}`}
     >
       <div className={`nc-MainNav1 relative z-10 ${className}`}>
-        <div className="px-6 py-4 lg:py-2 relative flex justify-between items-center">
-          <div className="flex justify-start flex-1 items-center gap-[15px]">
-            <div className="flex gap-3">
-              <NavLink
-                to="/dashboard"
-                className="main-logo flex items-center shrink-0 gap-2"
-              >
-                <img
-                  className="w-[80px]"
-                  src={'/icons/logo-app-trim.png'}
-                  alt="Logo"
-                />
-              </NavLink>
-            </div>
+        <div className="px-6 py-4 lg:py-2 relative flex items-center">
+          <div className="flex gap-3">
+            <NavLink
+              to="/dashboard"
+              className="main-logo flex items-center shrink-0 gap-2"
+            >
+              <img
+                className="w-[80px]"
+                src={'/icons/logo-app-trim.png'}
+                alt="Logo"
+              />
+            </NavLink>
           </div>
-          <ul className="links flex gap-3 pr-8">
-            {navigation.map((option, index) => (
-              <li
-                key={index}
-                className="cursor-pointer flex items-center w-full rounded-md hover:bg-base-200 "
-              >
-                <NavLink
-                  to={option.url || ''}
-                  className={({ isActive }) =>
-                    `w-full group flex items-center px-3 py-2 gap-3 ${
-                      isActive ? 'text-primary-500 bg-base-100 rounded-md' : ''
-                    }`
-                  }
+          <div className="w-full flex justify-center px-3">
+            <ul className="links flex gap-3 pr-8">
+              {navigation.map((option, index) => (
+                <li
+                  key={index}
+                  className="cursor-pointer flex items-center w-full rounded-md hover:bg-base-200 "
                 >
-                  <Text className="font-light" size="1">
-                    {option.text}
-                  </Text>
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-          {children}
+                  <NavLink
+                    to={option.url || ''}
+                    className={({ isActive }) =>
+                      `w-full group flex items-center px-3 py-2 gap-3 ${
+                        isActive
+                          ? 'text-primary-500 bg-base-100 rounded-md'
+                          : ''
+                      }`
+                    }
+                  >
+                    <Text className="font-light" size="1">
+                      {option.text}
+                    </Text>
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>{children}</div>
         </div>
       </div>
     </div>
