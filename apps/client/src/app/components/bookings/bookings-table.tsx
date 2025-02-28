04e9690ff6c3f5029435d9708269b5c9457e1d4e
Lenin Capa <leviskp87@gmail.com>
2025-02-28 13:41:04 -0500

    update table bookings

--- apps/client/src/app/components/bookings/bookings-table.tsx
+++ apps/client/src/app/components/bookings/bookings-table.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import moment from 'moment';
-import { Text } from '@magnetic/ui';
+import { Button, Text } from '@magnetic/ui';
 import { useBookings } from '../../hooks/userBookings';
 
 interface Props {}
@@ -15,10 +15,11 @@ export function BookingsTable(props: Props) {
       <table className="table w-full min-w-[600px]">
         <thead>
           <tr>
-            <th>Booking</th>
+            <th>Date</th>
             <th>Service</th>
-            <th>Client</th>
-            <th>Order</th>
+            <th>Booking</th>
+            <th>Status</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -26,25 +27,24 @@ export function BookingsTable(props: Props) {
             bookings.map((booking, index) => (
               <tr className="hover" key={index}>
                 <td>
-                  <Link
-                    className="hover:text-primary-500 hover:underline"
-                    to={`/orders/${booking.order.id}`}
-                  >
-                    {bookingsOptions[index]?.label || `Order #${booking.id}`}
-                  </Link>
                   <Text size="1" className="text-gray-500">
                     {moment(booking.createdAt).format('DD MMM YYYY')}
                   </Text>
                 </td>
                 <td>{booking.service.name}</td>
-                <td>{booking.order.user.name}</td>
                 <td>
                   <Link
                     className="hover:text-primary-500 hover:underline"
                     to={`/orders/${booking.order.id}`}
                   >
-                    {`#${booking.order.id}`}
+                    {`#${booking.id}`}
                   </Link>
+                </td>
+                <td>{booking.order.status}</td>
+                <td>
+                  <Button variant="outline" radius="full">
+                    Modify
+                  </Button>
                 </td>
               </tr>
             ))}
