fbe014f58c2f9df334c9efb7cd10563c77592f13
Lenin Capa <leviskp87@gmail.com>
2025-03-03 17:43:04 -0500

    integrate ical events with availability api

--- apps/magnetic/src/app/api/v1/user/boats/availability/route.ts
+++ apps/magnetic/src/app/api/v1/user/boats/availability/route.ts
@@ -1,5 +1,6 @@
 import db from 'apps/magnetic/src/app/libs/db';
 import { NextRequest, NextResponse } from 'next/server';
+import ical from 'ical';
 
 export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl;
@@ -14,14 +15,44 @@ export async function GET(request: NextRequest) {
     );
   }
 
+  const boat = await db.boatAttributes.findUnique({
+    where: { id: Number(boatId) },
+  });
+
+  if (!boat) {
+    return NextResponse.json({ message: 'Missing boat' }, { status: 400 });
+  }
+
+  let icalEvents: { startDate: Date; endDate: Date; source: string }[] = [];
+  const fromDate = new Date(from);
+  const toDate = new Date(to);
+
+  if (boat.iCal) {
+    try {
+      const response = await fetch(boat.iCal);
+      const iCalData = await response.text();
+      const events = ical.parseICS(iCalData);
+
+      icalEvents = Object.values(events)
+        .filter((event) => event.type === 'VEVENT' && event.start && event.end)
+        .map((event) => ({
+          startDate: new Date(event.start || ''),
+          endDate: new Date(event.end || ''),
+          source: 'ical',
+        }))
+        .filter(
+          (event) => event.startDate >= fromDate && event.endDate <= toDate
+        ); // Filtrar por rango de fechas
+    } catch (error) {
+      console.error('Error fetching or parsing iCal:', error);
+    }
+  }
+
   try {
-    const results = await db.boatAvailability.findMany({
+    const dbEvents = await db.boatAvailability.findMany({
       where: {
-        boatId: Number(boatId),
-        AND: [
-          { startDate: { gte: new Date(from) } },
-          { endDate: { lte: new Date(to) } },
-        ],
+        boatId: boat.id,
+        AND: [{ startDate: { gte: fromDate } }, { endDate: { lte: toDate } }],
       },
       select: {
         startDate: true,
@@ -30,7 +61,13 @@ export async function GET(request: NextRequest) {
       orderBy: { startDate: 'asc' },
     });
 
-    return NextResponse.json(results);
+    const formattedDbEvents = dbEvents.map((event) => ({
+      startDate: event.startDate,
+      endDate: event.endDate,
+      source: 'app',
+    }));
+
+    return NextResponse.json([...formattedDbEvents, ...icalEvents]); // Unificar eventos de DB e iCal
   } catch (error: any) {
     return NextResponse.json(
       {
