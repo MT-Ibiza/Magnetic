1bdf3972494859b605ebc352e29be222b5c68f8a
Lenin Capa <leviskp87@gmail.com>
2025-03-06 20:59:48 -0500

    remove conditions

--- apps/client/src/app/pages/view-item-page/view-boat.tsx
+++ apps/client/src/app/pages/view-item-page/view-boat.tsx
@@ -15,7 +15,12 @@ import MobileItemSticky from '../../components/mobile-footer-item';
 import BoatCalendar from './boat-calendar';
 import Modal from '../../components/modal';
 import RenderBookingForm from '../../components/services/booking-forms/render-booking-form';
-import { FormSubmitParams, Item, SeasonPrice } from '@magnetic/interfaces';
+import {
+  Boat,
+  FormSubmitParams,
+  Item,
+  SeasonPrice,
+} from '@magnetic/interfaces';
 import { useApp } from '../../hooks/useApp';
 import moment from 'moment';
 import BookBoatCard from './book-boat-card';
@@ -36,7 +41,18 @@ export function ViewBoat({ item }: Props) {
   const [startDate, setStartDate] = useState<Date | null>(initialDate);
   const [openFormModal, setOpenFormModal] = useState(false);
   const { setSelectedItem } = useApp();
-  const { seasonPrices, priceInCents } = item;
+  const { seasonPrices, priceInCents, service } = item;
+  const boat = item.boatAttributes as Boat;
+  const {
+    port,
+    secondName,
+    capacity,
+    crew,
+    lengthInMeters,
+    beamInMeters,
+    cabins,
+    fuelConsumption,
+  } = boat;
   const monthPrice = getSeasonPrice(seasonPrices, defaultMonthNumber);
   const displayPrice = monthPrice?.priceInCents ?? priceInCents;
   const [boatPrice, setBoatPrice] = useState(displayPrice);
@@ -54,26 +70,6 @@ export function ViewBoat({ item }: Props) {
     setTimeout(() => setAlert(null), 3000);
   };
 
-  const handleAddItem = (item: any, quantity: number) => {
-    const newVal = quantity + 1;
-    addServiceToCart.mutate(
-      { itemId: item.id, quantity: newVal },
-      {
-        onSuccess: () => {
-          addItem({
-            id: item.id,
-            item: item,
-            quantity: newVal,
-          });
-          showAlert('Product added to the cart', 'success');
-        },
-        onError: () => {
-          showAlert('Failed to add product to the cart', 'error');
-        },
-      }
-    );
-  };
-
   const onChangeDate = (dates: [Date | null, Date | null]) => {
     const [start, end] = dates;
     setStartDate(start);
@@ -83,38 +79,32 @@ export function ViewBoat({ item }: Props) {
     {
       name: 'Capacity',
       icon: <FaUsers className="text-lg" />,
-      value: item.boatAttributes?.capacity || 'n/a',
+      value: capacity || 'n/a',
     },
     {
       name: 'Crew',
       icon: <FaUserTie className="text-lg" />,
-      value: item.boatAttributes?.crew || 'n/a',
+      value: crew || 'n/a',
     },
     {
       name: 'Size',
       icon: <FaRulerCombined className="text-lg" />,
-      value: item.boatAttributes?.lengthInMeters
-        ? item.boatAttributes?.lengthInMeters + ' m'
-        : 'n/a',
+      value: lengthInMeters ? lengthInMeters + ' m' : 'n/a',
     },
     {
       name: 'Beam',
       icon: <FaShip className="text-lg" />,
-      value: item.boatAttributes?.beamInMeters
-        ? item.boatAttributes?.beamInMeters + ' m'
-        : 'n/a',
+      value: beamInMeters ? beamInMeters + ' m' : 'n/a',
     },
     {
       name: 'Cabins',
       icon: <FaBed className="text-lg" />,
-      value: item.boatAttributes?.cabins || 'n/a',
+      value: cabins || 'n/a',
     },
     {
       name: 'Fuel Consumption',
       icon: <FaGasPump className="text-lg" />,
-      value: item.boatAttributes?.fuelConsumption
-        ? item.boatAttributes?.beamInMeters + ' L/H'
-        : 'n/a',
+      value: fuelConsumption ? beamInMeters + ' L/H' : 'n/a',
     },
   ];
 
@@ -154,34 +144,28 @@ export function ViewBoat({ item }: Props) {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <div className="col-span-2 space-y-6">
           <div className="listingSection__wrap bg-base-100 !space-y-6">
-            {item.boatAttributes?.boatType && (
-              <div className="flex justify-between items-center">
-                <Badge name={item.boatAttributes.port} />
-              </div>
-            )}
+            <div className="flex justify-between items-center">
+              <Badge name={port} />
+            </div>
             <div>
               <h2 className="text-2xl sm:text-3xl lg:text-4xl font-semibold">
                 {item.name}
               </h2>
               <p className="text-l sm:text-xl lg:text-xl font-semibold text-gray-500 mt-2">
-                {item.boatAttributes?.secondName}
+                {secondName}
               </p>
             </div>
-            {item.boatAttributes && (
-              <>
-                <div className="w-full border-b border-neutral-100 dark:border-neutral-700" />
-                <div className="grid grid-cols-2 xl:grid-cols-3 gap-6 text-sm text-neutral-700 dark:text-neutral-300 ">
-                  {BoatAttributes.map((amenity, index) => (
-                    <div key={index} className="flex items-center space-x-3">
-                      {amenity.icon}
-                      <span>
-                        {amenity.name}: {amenity.value}
-                      </span>
-                    </div>
-                  ))}
+            <div className="w-full border-b border-neutral-100 dark:border-neutral-700" />
+            <div className="grid grid-cols-2 xl:grid-cols-3 gap-6 text-sm text-neutral-700 dark:text-neutral-300 ">
+              {BoatAttributes.map((amenity, index) => (
+                <div key={index} className="flex items-center space-x-3">
+                  {amenity.icon}
+                  <span>
+                    {amenity.name}: {amenity.value}
+                  </span>
                 </div>
-              </>
-            )}
+              ))}
+            </div>
           </div>
           <SectionCard title="Included">
             <div className="text-neutral-6000 dark:text-neutral-300 editor-text">
@@ -193,93 +177,83 @@ export function ViewBoat({ item }: Props) {
               />
             </div>
           </SectionCard>
-          {item.boatAttributes && (
-            <>
-              <SectionCard title="Calendar" subTitle="Select date">
-                <BoatCalendar
-                  startDate={startDate}
-                  onChangeDate={(dates) => {
-                    if (dates && dates[0]) {
-                      const date = dates[0];
-                      const monthNumber = getNumberMonth(date);
-                      const season = getSeasonPrice(seasonPrices, monthNumber);
-                      season && setBoatPrice(season.priceInCents);
-                    }
-                    onChangeDate(dates);
-                  }}
-                  boatId={item.boatAttributes.id}
-                />
-              </SectionCard>
-              <SectionCard title="Location" subTitle={item.boatAttributes.port}>
-                <div className="aspect-w-5 aspect-h-5 sm:aspect-h-3 ring-1 ring-black/10 rounded-xl z-0">
-                  <div className="rounded-xl overflow-hidden z-0">
-                    <iframe
-                      title="Mapa de la Torre Eiffel"
-                      width="100%"
-                      height="450px"
-                      loading="lazy"
-                      allowFullScreen
-                      referrerPolicy="no-referrer-when-downgrade"
-                      src="https://maps.google.com/maps?q=48.8584,2.2945&z=14&output=embed"
-                    ></iframe>
-                  </div>
-                </div>
-              </SectionCard>
-              {(item.service?.instructions ||
-                item.service?.termsAndConditions) && (
-                <SectionCard title="Need to Know">
-                  <div>
-                    {item.service.instructions && (
-                      <>
-                        <h4 className="text-lg font-semibold">
-                          Before You Book
-                        </h4>
-                        <div
-                          className="editor-text block mt-3 leading-relaxed text-neutral-500 dark:text-neutral-400"
-                          dangerouslySetInnerHTML={{
-                            __html: item.service.instructions,
-                          }}
-                        />
-                      </>
-                    )}
-                    {item.service.instructions &&
-                      item.service.termsAndConditions && (
-                        <div className="w-14 my-[32px] border-b border-neutral-200 dark:border-neutral-700"></div>
-                      )}
-                    {item.service.termsAndConditions && (
-                      <>
-                        <h4 className="text-lg font-semibold">
-                          Cancellation Policy
-                        </h4>
-                        <div
-                          className="editor-text block mt-3 leading-relaxed text-neutral-500 dark:text-neutral-400"
-                          dangerouslySetInnerHTML={{
-                            __html: item.service.termsAndConditions,
-                          }}
-                        />
-                      </>
-                    )}
-                  </div>
-                </SectionCard>
-              )}
-            </>
+          <SectionCard title="Calendar" subTitle="Select date">
+            <BoatCalendar
+              startDate={startDate}
+              onChangeDate={(dates) => {
+                if (dates && dates[0]) {
+                  const date = dates[0];
+                  const monthNumber = getNumberMonth(date);
+                  const season = getSeasonPrice(seasonPrices, monthNumber);
+                  season && setBoatPrice(season.priceInCents);
+                }
+                onChangeDate(dates);
+              }}
+              boatId={boat.id}
+            />
+          </SectionCard>
+          <SectionCard title="Location" subTitle={port}>
+            <div className="aspect-w-5 aspect-h-5 sm:aspect-h-3 ring-1 ring-black/10 rounded-xl z-0">
+              <div className="rounded-xl overflow-hidden z-0">
+                <iframe
+                  title={port}
+                  width="100%"
+                  height="450px"
+                  loading="lazy"
+                  allowFullScreen
+                  referrerPolicy="no-referrer-when-downgrade"
+                  src="https://maps.google.com/maps?q=48.8584,2.2945&z=14&output=embed"
+                ></iframe>
+              </div>
+            </div>
+          </SectionCard>
+          {(service.instructions || service.termsAndConditions) && (
+            <SectionCard title="Need to Know">
+              <div>
+                {service.instructions && (
+                  <>
+                    <h4 className="text-lg font-semibold">Before You Book</h4>
+                    <div
+                      className="editor-text block mt-3 leading-relaxed text-neutral-500 dark:text-neutral-400"
+                      dangerouslySetInnerHTML={{
+                        __html: service.instructions,
+                      }}
+                    />
+                  </>
+                )}
+                {service.instructions && service.termsAndConditions && (
+                  <div className="w-14 my-[32px] border-b border-neutral-200 dark:border-neutral-700"></div>
+                )}
+                {service.termsAndConditions && (
+                  <>
+                    <h4 className="text-lg font-semibold">
+                      Cancellation Policy
+                    </h4>
+                    <div
+                      className="editor-text block mt-3 leading-relaxed text-neutral-500 dark:text-neutral-400"
+                      dangerouslySetInnerHTML={{
+                        __html: service.termsAndConditions,
+                      }}
+                    />
+                  </>
+                )}
+              </div>
+            </SectionCard>
           )}
         </div>
         <div className="hidden lg:block col-span-1">
-          {item && (
-            <BookBoatCard
-              price={boatPrice}
-              startDate={startDate}
-              item={item}
-              onClick={() => {
-                setOpenFormModal(true);
-                setSelectedItem(item);
-              }}
-            />
-          )}
+          <BookBoatCard
+            price={boatPrice}
+            startDate={startDate}
+            item={item}
+            onClick={() => {
+              setOpenFormModal(true);
+              setSelectedItem(item);
+            }}
+          />
         </div>
         <div className="block lg:hidden">
-          {item && <MobileItemSticky startDate={startDate} item={item} />}
+          <MobileItemSticky startDate={startDate} item={item} />
         </div>
       </div>
       {alert && (
