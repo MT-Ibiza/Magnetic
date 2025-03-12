fab0df28a8ac9b71fce01cfaddce138c1d2c577c
Lenin Capa <leviskp87@gmail.com>
2025-03-12 16:55:48 -0500

    show options to mark booking status

--- apps/admin/src/app/pages/booking-page/booking-page.tsx
+++ apps/admin/src/app/pages/booking-page/booking-page.tsx
@@ -4,11 +4,10 @@ import {
   StatusBooking,
   Text,
 } from '@magnetic/ui';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useBooking } from '../../hooks/useBooking';
 import Loading from '../../components/loading';
 import { ErrorText } from '../../components/error-text';
-import { formatDate } from '../../utils';
 
 export function BookingPage() {
   const params = useParams();
@@ -30,10 +29,14 @@ export function BookingPage() {
         <div className="flex justify-between">
           <div className="flex flex-col gap-1">
             <h2 className="text-2xl font-semibold">Booking #{bookingId}</h2>
-            <p className="text-sm text-gray-500 ">
-              Date:{' '}
-              {data?.booking?.date ? formatDate(data.booking?.date) : 'n/a'}
-            </p>
+            <Text className="text-sm text-gray-500 ">
+              Client: {data?.user.name}
+            </Text>
+            <Link to={`/orders/${data?.booking.orderId}`}>
+              <Text className="text-sm text-gray-500">
+                Order: #{data?.booking.orderId}
+              </Text>
+            </Link>
           </div>
           <div>
             <StatusBooking status={data?.booking?.status || ''} />
@@ -43,12 +46,34 @@ export function BookingPage() {
           <div className="">
             {data.booking.modificationRequest &&
               data.booking.status === 'modification_requested' && (
-                <div className="border p-5 rounded-md w-full mt-5">
-                  <h1 className="font-medium text-sm mb-2">
-                    {data.user.name} has required changes
-                  </h1>
-                  <div className="">
-                    <Text>{data.booking.modificationRequest}</Text>
+                <div className="flex justify-between items-center border p-5 rounded-md w-full mt-5">
+                  <div>
+                    <h1 className="font-medium text-sm mb-2">Message</h1>
+                    <div className="">
+                      <Text>{data.booking.modificationRequest}</Text>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="dropdown dropdown-bottom dropdown-end">
+                      <div
+                        tabIndex={0}
+                        role="button"
+                        className="border rounded-md bg-gray-100 p-2"
+                      >
+                        <Text size="1">Mark as</Text>
+                      </div>
+                      <ul
+                        tabIndex={0}
+                        className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+                      >
+                        <li>
+                          <Text size="1">✅ Approved</Text>
+                        </li>
+                        <li>
+                          <Text size="1">❌ Canceled</Text>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               )}
