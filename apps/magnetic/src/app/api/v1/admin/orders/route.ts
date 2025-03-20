0625ddc62325c3633b0ae95120519f16ce54a62d
Lenin Capa <leviskp87@gmail.com>
2025-03-19 23:16:11 -0500

    check error in production

--- apps/magnetic/src/app/api/v1/admin/orders/route.ts
+++ apps/magnetic/src/app/api/v1/admin/orders/route.ts
@@ -1,45 +1,37 @@
 import db from 'apps/magnetic/src/app/libs/db';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic'; // Desactiva la optimización estática
+
 export async function GET(request: Request) {
-  try {
-    const orders = await db.order.findMany({
-      include: {
-        user: {
-          select: { id: true, name: true, email: true },
-        },
-        items: {
-          include: {
-            item: {
-              select: {
-                id: true,
-                name: true,
-                description: true,
-                priceInCents: true,
-              },
+  const orders = await db.order.findMany({
+    include: {
+      user: {
+        select: { id: true, name: true, email: true },
+      },
+      items: {
+        include: {
+          item: {
+            select: {
+              id: true,
+              name: true,
+              description: true,
+              priceInCents: true,
             },
           },
         },
       },
-      orderBy: {
-        createdAt: 'desc',
-      },
-    });
-
-    return NextResponse.json(orders, {
-      headers: {
-        'Cache-Control':
-          'no-store, no-cache, must-revalidate, proxy-revalidate',
-        Pragma: 'no-cache',
-        Expires: '0',
-      },
-    });
-  } catch (error: any) {
-    return NextResponse.json(
-      {
-        message: error.message || 'An unexpected error occurred',
-      },
-      { status: 500 }
-    );
-  }
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+  console.log('total orders: ', orders.length);
+  return NextResponse.json(orders, {
+    headers: {
+      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
+    },
+  });
 }
