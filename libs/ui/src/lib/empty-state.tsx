9a6a435c0ce398f68b24aa2c8676f17e2feb68c9
Lenin Capa <leviskp87@gmail.com>
2025-03-22 10:50:41 -0500

    remove border empty state

--- libs/ui/src/lib/empty-state.tsx
+++ libs/ui/src/lib/empty-state.tsx
@@ -14,10 +14,10 @@ export function EmptyState(props: EmptyStateProps) {
   const { className, title, icon: Icon, description, children } = props;
 
   return (
-    <div className="flex items-center mt-6 text-center border rounded-lg h-96 dark:border-gray-700">
+    <div className="flex items-center mt-6 text-center h-96">
       <div className="flex flex-col w-full max-w-sm px-4 mx-auto">
         {Icon && (
-          <div className="p-3 mx-auto text-primary-500 bg-primary-100 rounded-full dark:bg-gray-800">
+          <div className="p-3 mx-auto text-primary-500 bg-primary-100 rounded-full">
             <Icon size={45} className="text-primary-500" />
           </div>
         )}
