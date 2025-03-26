5b3fbf4fd80e0830a0961fd77e0e1e809b7aee0e
Lenin Capa <leviskp87@gmail.com>
2025-03-25 23:32:24 -0500

    improvements

--- apps/client/src/app/pages/view-service-page/filter-drinks.tsx
+++ apps/client/src/app/pages/view-service-page/filter-drinks.tsx
@@ -1,6 +1,6 @@
 import { CustomInput, SelectCategory } from '@magnetic/ui';
 import { useState } from 'react';
-import { LuWine } from 'react-icons/lu';
+import { IoSearch } from 'react-icons/io5';
 
 interface CategoriesSelect {
   name: string;
@@ -42,11 +42,13 @@ function FilterDrinks(props: Props) {
       <div className="border border-neutral-200 hidden lg:block sticky z-10 top-[80px] w-full relative mt-8 rounded-[40px] xl:rounded-[49px] shadow-xl dark:shadow-2xl bg-white dark:bg-neutral-800">
         <form className="lg:grid grid-cols-2 gap-x-[30px]">
           <CustomInput
-            icon={<LuWine className="w-5 h-5 lg:w-7 lg:h-7 text-neutral-400" />}
+            icon={
+              <IoSearch className="w-5 h-5 lg:w-7 lg:h-7 text-neutral-400" />
+            }
             allowTyping
-            placeHolder="Search by name"
+            placeHolder="Search"
             name="drink"
-            desc="Search your drinks"
+            desc="Search by name"
             options={[]}
             value={searchParams.drink}
             onChange={handleSearchChange}
