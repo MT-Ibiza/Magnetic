fa4360790c53c7db58741fb1ff0c0bf4c41a8a32
Lenin Capa <leviskp87@gmail.com>
2025-03-31 12:39:06 -0500

    new api to auto update calendars

---
+++ apps/magnetic/src/app/api/v1/calendars/update/route.ts
@@ -0,0 +1,87 @@
+import db from 'apps/magnetic/src/app/libs/db';
+import { NextResponse } from 'next/server';
+import ical from 'ical';
+
+export async function POST() {
+  try {
+    const boats = await db.boatAttributes.findMany({
+      select: {
+        id: true,
+        iCal: true,
+      },
+      where: {
+        iCal: { not: null },
+        item: {
+          published: true,
+        },
+      },
+    });
+
+    console.log(`Processing ${boats.length} boats with calendars`);
+
+    let updatedBoats = 0;
+
+    await Promise.all(
+      boats.map(async (boat) => {
+        try {
+          const response = await fetch(boat.iCal as string);
+          if (!response.ok) {
+            console.warn(`Failed to fetch iCal for boat ${boat.id}`);
+            return;
+          }
+
+          const iCalData = await response.text();
+          const events = ical.parseICS(iCalData);
+
+          const availability = Object.values(events)
+            .filter(
+              (event) => event?.type === 'VEVENT' && event.start && event.end
+            )
+            .map((event) => ({
+              boatId: boat.id,
+              startDate: event.start ? new Date(event.start) : new Date(),
+              endDate: event.end ? new Date(event.end) : new Date(),
+              text: event.summary || '',
+              source: 'ical',
+            }));
+
+          await db.boatAvailability.deleteMany({
+            where: {
+              boatId: boat.id,
+              source: 'ical',
+            },
+          });
+
+          for (const event of availability) {
+            const existingEvent = await db.boatAvailability.findFirst({
+              where: {
+                boatId: event.boatId,
+                startDate: event.startDate,
+                endDate: event.endDate,
+                source: 'ical',
+              },
+            });
+
+            if (!existingEvent) {
+              await db.boatAvailability.create({ data: event });
+            }
+          }
+
+          updatedBoats++;
+        } catch (error) {
+          console.error(`Error processing boat ${boat.id}:`, error);
+        }
+      })
+    );
+
+    return NextResponse.json({
+      message: `Updated ${updatedBoats} boats successfully`,
+    });
+  } catch (error) {
+    console.error('Unexpected error:', error);
+    return NextResponse.json(
+      { message: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}
