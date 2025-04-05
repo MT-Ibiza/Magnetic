7839713843ceede8fdc0112c90cf03f707419084
Lenin Capa <leviskp87@gmail.com>
2025-04-05 18:45:39 -0500

    remove table public drinks

--- libs/interfaces/src/lib/drinks.ts
+++ libs/interfaces/src/lib/drinks.ts
@@ -10,28 +10,3 @@ export interface DrinkSearchAttributes {
   categoriesIds?: string;
   name?: string;
 }
-
-export interface DrinksListItem {
-  id: number;
-  drinkItemListId: number;
-  item: Item;
-  itemId: number;
-}
-
-export interface DrinksListBase {
-  name: string;
-  slug: string;
-  description: string;
-  items: DrinksListItem[];
-}
-
-export interface DrinksList extends DrinksListBase {
-  id: number;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-export interface NewDrinksList {
-  name: string;
-  itemsIds: number[];
-}
