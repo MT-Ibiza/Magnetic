cd15f3a9a38fc17704a33392db077fcac2f3dbfc
Lenin Capa <leviskp87@gmail.com>
2025-04-09 12:27:14 -0500

    reduce time load search

--- apps/client/src/app/pages/view-service-page/list-boats.tsx
+++ apps/client/src/app/pages/view-service-page/list-boats.tsx
@@ -38,7 +38,7 @@ function ListBoats(props: Props) {
     queryKey: ['boats', searchParams],
     queryFn: async () => {
       const result = searchBoats(searchParams);
-      await new Promise((resolve) => setTimeout(resolve, 500));
+      await new Promise((resolve) => setTimeout(resolve, 300));
       return result;
     },
   });
